ef5588c10bad3d9d70c2941e549f1f97237b5c8e
Narendra Ghule <narendra.ghule@pentagon.co.in>
2023-11-07 12:52:36 +0530

    install dependencies changed

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -11,9 +11,11 @@ sudo apt-get install apache2 -y
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt-get -y install npm
+
 cd /var/www/html
 sudo rm -rf *
 cd /app
+
 sudo npm i
 sudo npm run build
 sudo cp -r build/. /var/www/html
