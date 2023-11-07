f73945589529d6e73d11fb4b4e70098e9df9b7a3
Narendra Ghule <narendra.ghule@pentagon.co.in>
2023-11-07 10:47:30 +0530

    Scripts upated

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # this file runs first, so during an initail installation error might happen
 
-# simply stop the application process using pm2
-#echo stopping server
-#sudo su
-#pm2 stop npm
-#exit
+sudo service apache2 stop
