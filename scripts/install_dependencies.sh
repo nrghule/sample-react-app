cc5e33b11bffc669267a421902f862575f7df018
Narendra Ghule <narendra.ghule@pentagon.co.in>
2023-11-22 15:59:16 +0530

    codedeploy script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -12,21 +12,10 @@ curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt-get -y install npm
 
-# # check to make sure the symbolic link for nodejs node exists
-echo checking for nodejs symlink
-file="/usr/bin/node"
-if [ -f $file ] && [ ! -L $file ] ; then
-  echo "$file exists and is not a symlink"
-  sudo ln -s /usr/bin/nodejs
-else
-  echo "$file exists and is already a symlink"
-fi
-
-# # install the application using npm
-# # we need to traverse to where the application bundle is copied too.
-# echo installing application with npm
 cd /var/www/html
-sudo npm install
+sudo rm -rf *
+cd /app
 
-echo installing pm2
-sudo npm install pm2 -g
+sudo npm i
+sudo npm run build
+sudo cp -r build/. /var/www/html
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 echo starting server
-
-# here we just use npm to run the build
-cd /app
-echo building application...
-sudo npm install
-sudo npm run build
-cd /var/www/html
-sudo rm -rf *
-sudo cp -r /app/build/. /var/www/html
+sudo service apache2 start
 # start the application with pm2
 echo starting application...
 #sudo pm2 start npm -- run production
