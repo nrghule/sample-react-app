73bcc2cec4b3c299f74a74aff8143acbbe782584
Narendra Ghule <narendra.ghule@pentagon.co.in>
2023-11-22 18:17:43 +0530

    update 6

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Update 5
+          Learn React Update 6
         </a>
       </header>
     </div>
