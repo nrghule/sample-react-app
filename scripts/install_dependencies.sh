b20c4c83e12d138540207b635550bc18c6953260
Narendra Ghule <narendra.ghule@pentagon.co.in>
2023-11-22 16:02:34 +0530

    codedeploy script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# this file is being executed in /opt/codedeploy-agent/deployment-root/47../<deployment_id>/
-
-#stdout logs of this process executing can be found in /opt/codedeploy-agent/deployment-root/47../<deployment_id>/logs/scripts.log
-
-# here we update the server and install node and npm
 echo installing dependencies
 sudo apt-get update
 sudo apt-get install apache2 -y
