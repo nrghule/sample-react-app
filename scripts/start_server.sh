13d9e716bdc1fe4eb469bd84c69ac69316b63a41
Narendra Ghule <narendra.ghule@pentagon.co.in>
2023-11-22 17:43:48 +0530

    updated script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 echo starting server
+cd /var/www/html
+sudo rm -rf *
+cd /app
+
+sudo npm i
+sudo npm run build
+sudo cp -r /app/build/. /var/www/html
+
 sudo service apache2 start
 # start the application with pm2
 echo starting application...
